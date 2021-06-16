687c23eaaeeac8d2eb71b1acfb03e1059e735ee9
최다인 <twa05157@gmail.com>
2021-06-17 02:16:48 +0900

    Fix: order controller & Feat: user controller

--- sungshin-shop/routes/home.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const homeController = require('../controller/homeController');
-const jwtMiddlewares = require('../middlewares/middlewares');
-
-router.get('/goodsHome', jwtMiddlewares.userJwt, homeController.goodsHome);
-
-module.exports = router;
